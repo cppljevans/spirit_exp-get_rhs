b60351daec50058156eee14bf28cb7195fb0fef5
Larry Evans <cppljevans@suddenlink.net>
2017-12-28 05:17:41 -0600

    Reformatted code in response to feedback from Nikita to PR#342. Change to iostreams_utility.hpp just to avoid private #include.

--- workbench/x3/rule_defns/gen_benchmark_gram.cpp
+++ workbench/x3/rule_defns/gen_benchmark_gram.cpp
@@ -18,16 +18,27 @@ public:
   std::ostream& operator++(){ _indent+=margin; return _os;}
   std::ostream& operator--(){ _indent-=margin; return _os;}
 };
+struct osb
+  //indented out stream block.
+  //CTOR indents
+  //DTOR dedents
+{
+private:
+  osi& _os;
+public:
+  osb(osi& os):_os(os)
+  { ++_os;}
+  ~osb()
+  { --_os;}
+};
 #include <string>
 
 #define DEFAULT_NUM_RULES 11
-  std::string
-r_(int ndx_rule)
+std::string r_(int ndx_rule)
 {
   return std::string("r_")+std::to_string(ndx_rule);
 }
-  std::string
-d_(int ndx_rule)
+std::string d_(int ndx_rule)
 {
   return r_(ndx_rule)+std::string("_def");
 }
@@ -35,64 +46,39 @@ enum rref_enum
 { rul
 , def
 };
-  template
-  < rref_enum Rref
-  >
-  std::string
-r_lhs
-  (int ndx_rule)
+template< rref_enum Rref>
+std::string r_lhs(int ndx_rule)
 {
   return std::string("r_")+std::to_string(ndx_rule);
 }
-  template
-  <
-  >
-  std::string
-r_lhs
-  < def
-  >
-  (int ndx_rule)
+template<>
+std::string r_lhs<def>(int ndx_rule)
 {
   return std::string("auto const ")+d_(ndx_rule)+"="+r_(ndx_rule);
 }
-  template
-  < rref_enum Rref
-  >
-  std::string
-r_rhs
-  (int ndx_rule)
+template< rref_enum Rref>
+std::string r_rhs(int ndx_rule)
 {
   return std::string("r_")+std::to_string(ndx_rule);
 }
-  template
-  <
-  >
-  std::string
-r_rhs
-  < def
-  >
-  (int ndx_rule)
+template<>
+std::string r_rhs< def>(int ndx_rule)
 {
   return d_(ndx_rule);
 }
-  std::string
-op(int ndx_rule)
+std::string op(int ndx_rule)
 {
   return std::string("string_(\"")+std::to_string(ndx_rule)+"\")";
 }
-  template
-  < rref_enum Rref
-  >
-  std::string
-rhs(int ndx_rule,int num_rules)
+template< rref_enum Rref>
+std::string rhs(int ndx_rule,int num_rules)
 {
   if(ndx_rule)
       return r_rhs<Rref>(ndx_rule-1)+">>*("+op(ndx_rule)+">>"+r_rhs<Rref>(ndx_rule-1)+")";
   else
     return "char_('_')|char_('[')>>"+r_(num_rules-1)+">>char_(']')";
 }
-  void
-rule_decls( osi&gfi, int num_rules)
+void rule_decls( osi&gfi, int num_rules)
 {
   for(int i_rule=0; i_rule<num_rules; ++i_rule)
     gfi()<<"rule_b<id<"<<i_rule<<">> const "<<r_(i_rule)<<";\n";
@@ -112,72 +98,59 @@ int main(int argc, char *argv[])
   gf.open("gram_file.hpp");
   {
     osi gfi(gf);
-    ++gfi;
-    //The following output pretty much follows the pattern
-    //in the rule_defns_benchmark.hpp in the #else part of
-    //the #ifdef USE_GRAM_FILE...#endif.
-    //
-    //The indentation probably seems odd; however, the
-    //reason for such indentation is to reflect the indentation
-    //produced on the output file.  It's much easier to debug
-    //this code because after a run, and checking where the output
-    //indentation is wrong, it's easier to see where a change
-    //in this code should be made, because there's a close
-    //correspondence between the indentation in this code
-    //and the indentation in the output file.
-    //
+    osb gfb(gfi);
     gfi()<<"#if RULE2RHS_WHICH != RULE2RHS_GET_RHS_CRTP\n";
-    ++gfi;
+    { osb gfb(gfi);
       rule_decls( gfi, num_rules);
-    --gfi;
+    }
     gfi()<<"#endif//RULE2RHS_WHICH != RULE2RHS_GET_RHS_CRTP\n";
     gfi()<<"#if USE_RHS_DEF"<<"\n";
-    ++gfi;
+    { osb gfb(gfi);
       for(i_rule=0;i_rule<num_rules; ++i_rule)
         gfi()<<r_lhs<def>(i_rule)+"="+rhs<def>(i_rule,num_rules)+";\n";
       gfi()<<"auto const& start="<<d_(num_rules-1)<<";\n";
       gfi()<<"#if RULE2RHS_WHICH == RULE2RHS_CTX_MI_REC\n";
-      ++gfi;
-          gfi()<<"auto const& ctx=make_defs\n";
-          gfi()<<"( "<<d_(num_rules-1)<<"\n";
-          gfi()<<");"<<"\n";
-      --gfi;
+      { osb gfb(gfi);
+        gfi()<<"auto const& ctx=make_defs\n";
+        gfi()<<"( "<<d_(num_rules-1)<<"\n";
+        gfi()<<");"<<"\n";
+      }
       gfi()<<"#else\n";
-        ++gfi;
+      { osb gfb(gfi);
         gfi()<<"auto const& ctx=empty_context();\n";
         gfi()<<"#if RULE2RHS_WHICH == RULE2RHS_GET_RHS_REC\n";
-          ++gfi;
+        { osb gfb(gfi);
           gfi()<<"BOOST_SPIRIT_DEFINE\n";
           gfi()<<"( "<<d_(num_rules-1)<<"\n";
           gfi()<<");"<<"\n";
-          --gfi;
+        }
         gfi()<<"#endif\n";
-        --gfi;
+        }
       gfi()<<"#endif //RULE2RHS_WHICH == RULE2RHS_CTX_MI_REC\n";
-    --gfi;
+    }
     gfi()<<"#elif RULE2RHS_WHICH != RULE2RHS_GET_RHS_CRTP\n";
-    ++gfi;
+    { osb gfb(gfi);
       for(i_rule=0;i_rule<num_rules; ++i_rule)
         gfi()<<r_lhs<def>(i_rule)+"="+rhs<rul>(i_rule,num_rules)+";\n";
       gfi()<<"auto const& start="<<d_(num_rules-1)<<";\n";
       gfi()<<"#if RULE2RHS_WHICH == RULE2RHS_CTX_MI_ALL\n";
-      ++gfi;
-          gfi()<<"auto const& ctx=make_defs\n";
-          for(i_rule=num_rules-1; i_rule>=0; --i_rule)
-          {
-            if(i_rule==num_rules-1)
-              gfi()<<"( ";
-            else
-              gfi()<<", ";  
-            gfi.os()<<d_(i_rule)<<"\n";
-          }
-          gfi()<<");"<<"\n";
-      --gfi;
+      { osb gfb(gfi);
+        gfi()<<"auto const& ctx=make_defs\n";
+        for(i_rule=num_rules-1; i_rule>=0; --i_rule)
+        {
+          if(i_rule==num_rules-1)
+            gfi()<<"( ";
+          else
+            gfi()<<", ";  
+          gfi.os()<<d_(i_rule)<<"\n";
+        }
+        gfi()<<");"<<"\n";
+      }
       gfi()<<"#else\n";
-        ++gfi;
+      { osb gfb(gfi);
         gfi()<<"auto const& ctx=empty_context();\n";
         gfi()<<"#if RULE2RHS_WHICH == RULE2RHS_GET_RHS_ALL\n";
-          ++gfi;
+        { osb gfb(gfi);
           gfi()<<"BOOST_SPIRIT_DEFINE\n";
           for(i_rule=num_rules-1; i_rule>=0; --i_rule)
           {
@@ -188,44 +161,39 @@ int main(int argc, char *argv[])
             gfi.os()<<d_(i_rule)<<"\n";
           }
           gfi()<<");"<<"\n";
-          --gfi;
+        }
         gfi()<<"#endif\n";
-        --gfi;
+      }
       gfi()<<"#endif //RULE2RHS_WHICH == RULE2RHS_CTX_MI_ALL\n";
-    --gfi;
+    }
     gfi()<<"#else//RULE2RHS_GET_RHS_CRTP\n";
-    ++gfi;
+    { osb gfb(gfi);
       gfi()<<"struct gram_deriv: gram_base<gram_deriv>\n";
       gfi()<<"{\n";
-      ++gfi;
+      { osb gfb(gfi);
         rule_decls( gfi, num_rules);
         gfi()<<"BOOST_SPIRIT_GET_RHS\n";
-        ++gfi;
-          gfi()<<"(\n";
-          ++gfi;
+        gfi()<<"( ";
+        { 
           for(i_rule=0; i_rule<num_rules; ++i_rule)
-          {
+          { 
             if(i_rule>0) 
-            {
-              --gfi; 
-              gfi()<<",\n";
-              ++gfi;
+            { 
+              gfi()<<", ";
             }
-            gfi()<<"(\n";
-            ++gfi;
-              gfi()<<r_lhs<rul>(i_rule)<<"="<<rhs<rul>(i_rule,num_rules)<<"\n";
-            --gfi;
+            gfi.os()<<"( ";
+            osb gfb(gfi);
+            gfi.os()<<r_lhs<rul>(i_rule)<<"="<<rhs<rul>(i_rule,num_rules)<<"\n";
             gfi()<<")\n";
           }
-          --gfi;
-          gfi()<<")\n";
-        --gfi;
-      --gfi;
+        }
+        gfi()<<")\n";
+      }
       gfi()<<"};\n";
       gfi()<<"gram_deriv g;\n";
       gfi()<<"auto const& start=g.get_rhs(id<"<<num_rules-1<<">{});\n";
       gfi()<<"auto const& ctx = empty_context();\n";
-    --gfi;
+    }
     gfi()<<"#endif//end:USE_RHS_DEF\n";
   }
   gf.close();
